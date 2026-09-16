7a1493fec856fd6e6d8fa75bcf9c85071016901e
Keshav Ghodake <keshavsghodake@gmail.com>
2026-09-16 08:10:23 +0000

    micro

--- models/date_dimention.sql
+++ models/date_dimention.sql
@@ -3,7 +3,8 @@ select
 started_at,
 date(started_at) as date_started_at,
 hour(started_at) as hour_started_at,
-dayname(started_at) as day_started_at
+{{day_type('STARTED_AT')}} AS DAY_TYPE,
+{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 from {{ source('demo', 'bike') }}
 )
 select 
